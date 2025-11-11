57df5bcb6f52e8dc11ec2bfdcad935632281c843
Marthín José Contreras Rodríguez <Marthin J Contreras>
2025-11-10 22:56:52 -0600

    Estilos de background de todo el proyecto, estilos del NavBar, implementacion de footer.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
-import { useState } from 'react'
 import './App.css'
 import { Link, Routes, Route } from 'react-router'
-import { BrowserRouter } from "react-router";
 import { Usuarios } from './pages/ListaUsuarios';
 import { Inicio } from './pages/Inicio';
 import { Productos } from './pages/Productos';
@@ -10,51 +8,43 @@ import { ConversorDivisa } from './Divisa'
 
 function App() {
   return (
-    <>
-
-<nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <Link className="navbar-brand" to="/">Seminario de Software</Link>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div className="navbar-nav">
-        <Link className="nav-link active" aria-current="page" to="/">Inicio</Link>
-        <Link className="nav-link" to="/clientes">Clientes</Link>
-        <Link className="nav-link" to="/divisa">Convertidor Divisa</Link>
-        <Link className="nav-link" to="/productos">Productos</Link>
-        {/* <Link className="nav-link" to="/acercade">Acerca De</Link> */}
-        <Link className="nav-link" to="/tiempo">Tiempo</Link>
-      </div>
-    </div>
-  </div>
-</nav>
-  <Routes>
-        <Route path='/' element={<Inicio/>} />
-        <Route path='/clientes' element={<Usuarios/> } />
-        <Route path='/divisa' element={<ConversorDivisa/> } />
-        <Route path='/productos' element={<Productos/> } />
-        <Route path='/tiempo' element={<Tiempo/> } />
-      </Routes>
-
-
-{/* 
-      <nav>
-        <Link to={"/"}>Inicio</Link>
-        <Link to={"/clientes"}>Clientes</Link>
-        <Link to={"/productos"}>Productos</Link>
-        <Link to={"/acercade"}>Acerca De</Link>
-        <Link to={"/tiempo"}>Tiempo</Link>
+    <div className="app-container">
+      {/* Navbar centrado y responsive */}
+      <nav className="navbar navbar-expand-lg navbar-custom">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">Seminario de Software</Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav mx-auto">
+              <Link className="nav-link" to="/">Inicio</Link>
+              <Link className="nav-link" to="/clientes">Clientes</Link>
+              <Link className="nav-link" to="/divisa">Convertidor Divisa</Link>
+              <Link className="nav-link" to="/productos">Productos</Link>
+              <Link className="nav-link" to="/tiempo">Tiempo</Link>
+            </div>
+          </div>
+        </div>
       </nav>
 
-      <Routes>
-        <Route path='/' element={<Inicio/>} />
-        <Route path='/clientes' element={<Usuarios/> } />
-        <Route path='/productos' element={<Productos/> } />
-      </Routes> */}
+      <main className="main-content">
+        <Routes>
+          <Route path='/' element={<Inicio/>} />
+          <Route path='/clientes' element={<Usuarios/> } />
+          <Route path='/divisa' element={<ConversorDivisa/> } />
+          <Route path='/productos' element={<Productos/> } />
+          <Route path='/tiempo' element={<Tiempo/> } />
+        </Routes>
+      </main>
 
-    </>
+      {/* Footer */}
+      <footer className="footer-custom">
+        <div className="container-fluid">
+          <p>SEMINARIO-TALLER DE SOFTWARE - Todos los Derechos reservados - Marthin Contreras</p>
+        </div>
+      </footer>
+    </div>
   )
 }
 
